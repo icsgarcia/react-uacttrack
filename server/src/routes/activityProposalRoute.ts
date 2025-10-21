17da6ac7a29320332aa8d58b7cb32ab828efba53
Ivan Garcia <icsgarcia2002@gmail.com>
2025-10-21 10:12:00 +0800

    updated User and ActivityProposal model and Added Organization model

--- server/src/routes/activityProposalRoute.ts
+++ server/src/routes/activityProposalRoute.ts
@@ -6,7 +6,7 @@ const activityProposalRouter = Router();
 
 const upload = multer({
     storage: multer.memoryStorage(),
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+    limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 activityProposalRouter.post(
