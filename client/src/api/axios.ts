2159d77c495bcccdcc26e2e1a9c400fde9a96f11
Ivan Garcia <icsgarcia2002@gmail.com>
2025-11-17 16:27:35 +0800

    update axios instance

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const LOCAL_BASE_URL = "http://localhost:3000";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.API_URL || LOCAL_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL || LOCAL_BASE_URL,
     headers: { "Content-Type": "application/json" },
     timeout: 5000,
     withCredentials: true,
